70ecbcc4f2f7cdb022f683691e3758217a6163ae
ajjju1 <ajmalmalik5783@gmail.com>
2020-03-15 02:43:28 +0530

    tweaked views page

--- website/views.py
+++ website/views.py
@@ -75,7 +75,7 @@ def subtract(request):
 
         correct_answer = int(old_num_1) - int(old_num_2)
         if int(answer) == correct_answer:
-            my_answer = "Correct! " +  old_num_1 + " + " + old_num_2 + " = " + answer 
+            my_answer = "Correct! " +  old_num_1 + " - " + old_num_2 + " = " + answer 
             color = "success"
         else:
             my_answer = "Incorrect! " +  old_num_1 + " - " + old_num_2 + " is not " + answer + " it is " + str(correct_answer)
@@ -119,7 +119,7 @@ def multiply(request):
 
         correct_answer = int(old_num_1) * int(old_num_2)
         if int(answer) == correct_answer:
-            my_answer = "Correct! " +  old_num_1 + " + " + old_num_2 + " = " + answer 
+            my_answer = "Correct! " +  old_num_1 + " x " + old_num_2 + " = " + answer 
             color = "success"
         else:
             my_answer = "Incorrect! " +  old_num_1 + " x " + old_num_2 + " is not " + answer + " it is " + str(correct_answer)
@@ -167,7 +167,7 @@ def divide(request):
 
         correct_answer = int(old_num_1) / int(old_num_2)
         if float(answer) == correct_answer:
-            my_answer = "Correct! " +  old_num_1 + " + " + old_num_2 + " = " + answer 
+            my_answer = "Correct! " +  old_num_1 + " / " + old_num_2 + " = " + answer 
             color = "success"
         else:
             my_answer = "Incorrect! " +  old_num_1 + " / " + old_num_2 + " is not " + answer + " it is " + str(correct_answer)
